a72a894adf4846cd520be09ae5b8bd7c5a54c0af
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-27 17:55:21 +0300

    Create card flip animation

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import cards from "./cards.js";
 
 const states = {
   flippedCards: 0,
-}
+};
 
 const allGameCards = [...cards, ...cards];
 
@@ -15,13 +15,15 @@ function renderCards(cardsObj) {
   let cardsHTML = cardsObj
     .map(({ name, imageSrc }) => {
       return `
-      <li class="card">
-        <div class="card-front">
-          <img class="card-img-front" src="images/treble_clef.webp" alt="image of treble clef">
-        </div>
-        <div class="card-back">
-          <img class="card-img" src="${imageSrc}" alt="image of ${name}">
-          <p class='card-desc'>${name}</p>
+      <li class="game-card">
+        <div class='card-inner'>
+          <div class="card-front">
+            <img class="card-img-front" src="images/treble_clef.webp" alt="image of treble clef">
+          </div>
+          <div class="card-back">
+            <img class="card-img" src="${imageSrc}" alt="image of ${name}">
+            <p class="card-desc">${name}</p>
+          </div>
         </div>
       </li>
       `;
