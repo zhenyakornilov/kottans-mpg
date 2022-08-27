da326ec81784d873d071b095255432279ec8112b
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-27 15:00:01 +0300

    Change card html structure, add front card image

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 import cards from "./cards.js";
 
+const states = {
+  flippedCards: 0,
+}
+
 const allGameCards = [...cards, ...cards];
+
+allGameCards.sort(() => {
+  return 0.5 - Math.random();
+});
 
 function renderCards(cardsObj) {
   const cardsAll = document.getElementById("game-cards");
   let cardsHTML = cardsObj
     .map(({ name, imageSrc }) => {
-      return (
-        `<li class="card">` +
-        `  <img class="card-img" src="${imageSrc}" alt="image of ${name}">` +
-        `  <p class='card-desc'>${name}</p>` +
-        `</li>`
-      );
+      return `
+      <li class="card">
+        <div class="card-front">
+          <img class="card-img-front" src="images/treble_clef.svg" alt="image of treble clef">
+        </div>
+        <div class="card-back">
+          <img class="card-img" src="${imageSrc}" alt="image of ${name}">
+          <p class='card-desc'>${name}</p>
+        </div>
+      </li>
+      `;
     })
     .join("");
 
